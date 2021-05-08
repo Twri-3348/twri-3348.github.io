3d63469b4ef0979f65cc6934170e6719525e7bc5
Twri-3348 <57317414+Twri-3348@users.noreply.github.com>
2021-05-09 02:21:56 +0300

    Code cleanup, design and accessibility

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  var element = document.getElementById("mobile-nav");
+  element.classList.toggle("hidden");
+}
+
+function closeMenu() {
+  var element = document.getElementById("mobile-nav");
+  element.classList.add("hidden");
+}
